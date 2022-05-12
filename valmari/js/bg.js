1e18a03fc1cbef8cb3e3b13bc070101fc7f422c0
XManit <104674599+XManit@users.noreply.github.com>
2022-05-12 13:30:46 +0500

    anim fix

--- valmari/js/bg.js
+++ valmari/js/bg.js
@@ -1,21 +1,13 @@
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-async function demo() {
-    console.log('Taking a break...');
-    await sleep(2000);
-    console.log('Two second later');
-}
 function my3() {
     document.querySelector(".img__sec1").innerHTML = "<img src='aylin/3 (3).jpg' alt=''>"
-    setTimeout(my1, 2000)
+    setTimeout(my1, 5000)        
 }
 function my2() {
     document.querySelector(".img__sec1").innerHTML = "<img src='aylin/byRanoGafurova-70323.jpg' alt=''>"
-    setTimeout(my3, 2000);
+    setTimeout(my3, 5000);
 }
 function my1() {
     document.querySelector(".img__sec1").innerHTML = "<img src='aylin/byRanoGafurova-70627.jpg' alt=''>"
-    setTimeout(my2, 2000);
+    setTimeout(my2, 5000);
 }
 my1()
