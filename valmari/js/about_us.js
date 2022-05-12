aa7ade649002cab73f68875dc8fe7f8077fc9140
XManit <104674599+XManit@users.noreply.github.com>
2022-05-12 13:48:14 +0500

    html update

--- valmari/js/about_us.js
+++ valmari/js/about_us.js
@@ -1,6 +1,4 @@
-const about_us = document.querySelector(".about_us");
-
-about_us.addEventListener("click", function about() {
+function about() {
     document.querySelector("main").innerHTML = `
             <div class="hero">
                 <div class="main__container container">
@@ -59,115 +57,8 @@ about_us.addEventListener("click", function about() {
                                 красоты, здоровья молодости. <br>
 
                             </p>
-                            <a href="#">
-                                <h6>
-                                    Подробнее ➡
-                                </h6>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section class="section-2">
-                <div class="container">
-                    <div class="section-2__title title">
-                        <h4>ВСЕГДА ЗАБОТЛИВЫЕ</h4>
-                        <h3>Наши Специальности</h3>
-                    </div>
-                    <div class="section-2__grid">
-                        <a href="#" class="grid__item">
-                            <div class="impulse_heart"></div>
-                            <p>Неврология</p>
-                        </a>
-                        <a href="#" class="grid__item">
-                            <div class="impulse_heart"></div>
-                            <p>Кости</p>
-                        </a>
-                        <a href="#" class="grid__item">
-                            <div class="impulse_heart"></div>
-                            <p>Онкология</p>
-                        </a>
-                        <a href="#" class="grid__item">
-                            <div class="impulse_heart"></div>
-                            <p>Оториноларингология</p>
-                        </a>
-                        <a href="#" class="grid__item">
-                            <div class="impulse_heart"></div>
-                            <p>Офтальмология</p>
-                        </a>
-                        <a href="#" class="grid__item">
-                            <div class="impulse_heart"></div>
-                            <p>Кардиоваскуляр</p>
-                        </a>
-                        <a href="#" class="grid__item">
-                            <div class="impulse_heart"></div>
-                            <p>Пульмонология</p>
-                        </a>
-                        <a href="#" class="grid__item">
-                            <div class="impulse_heart"></div>
-                            <p>Почечная медицина</p>
-                        </a>
-                        <a href="#" class="grid__item">
-                            <div class="impulse_heart"></div>
-                            <p>гастроэнтерология</p>
-                        </a>
-                        <a href="#" class="grid__item">
-                            <div class="impulse_heart"></div>
-                            <p>Урология</p>
-                        </a>
-                        <a href="#" class="grid__item">
-                            <div class="impulse_heart"></div>
-                            <p>Дерматология</p>
-                        </a>
-                        <a href="#" class="grid__item">
-                            <div class="impulse_heart"></div>
-                            <p>Гинекология</p>
-                        </a>
-                    </div>
-                </div>
-            </section>
-
-            <!-- section last -->
-
-            <section class="section__last">
-                <div class="container">
-                    <div class="title">
-                        <h4>Связаться</h4>
-                        <h3>Контакт</h3>
-                    </div>
-                    <div class="section__cards">
-                        <div class="card">
-                            <div class="call__icon"></div>
-                            <h4 class="last__title">ЭКСТРЕННЫЙ</h4>
-                            <a href="#" class="last__subtitle">(237) 681-812-255</a>
-                            <br>
-                            <a href="#" class="last__subtitle">(237) 666-331-894</a>
-                        </div>
-                        <div class="card">
-                            <div class="location__icon"></div>
-                            <h4 class="last__title">ЛОКАЦИЯ</h4>
-                            <p class="last__subtitle">0123 Где-то</p>
-                            <p class="last__subtitle">
-                                9876 Какая-то страна
-                            </p>
-                        </div>
-                        <div class="card">
-                            <div class="email__icon"></div>
-                            <h4 class="last__title">
-                                ЭЛЕКТРОННЫЙ АДРЕС
-                            </h4>
-                            <a href="#" class="last__subtitle">пример@почты.com</a>
-                            <a href="#" class="last__subtitle">пример@почты.com</a>
-                        </div>
-                        <div class="card">
-                            <div class="clock__icon"></div>
-                            <h4 class="last__title">
-                                РАБОЧИЕ ДНИ
-                            </h4>
-                            <a href="#" class="last__subtitle">Пон-Суб 09:00-20:00</a>
-                            <a href="#" class="last__subtitle">Воскресенье отдых</a>
                         </div>
                     </div>
                 </div>
             </section>`;
-});
+}
--- valmari/js/bg.js
+++ valmari/js/bg.js
@@ -1,13 +1,13 @@
 function my3() {
-    document.querySelector(".img__sec1").innerHTML = "<img src='aylin/3 (3).jpg' alt=''>"
-    setTimeout(my1, 5000)        
+    document.querySelector(".img__sec1").innerHTML = "<img src='aylin/3 (3).jpg' alt=''>";
+    setTimeout(my1, 5000);
 }
 function my2() {
-    document.querySelector(".img__sec1").innerHTML = "<img src='aylin/byRanoGafurova-70323.jpg' alt=''>"
+    document.querySelector(".img__sec1").innerHTML = "<img src='aylin/byRanoGafurova-70323.jpg' alt=''>";
     setTimeout(my3, 5000);
 }
 function my1() {
-    document.querySelector(".img__sec1").innerHTML = "<img src='aylin/byRanoGafurova-70627.jpg' alt=''>"
+    document.querySelector(".img__sec1").innerHTML = "<img src='aylin/byRanoGafurova-70627.jpg' alt=''>";
     setTimeout(my2, 5000);
 }
-my1()
+my1();
